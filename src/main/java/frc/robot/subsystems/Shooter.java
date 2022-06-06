1b36071f8e16c1a6126721f1c49caab4109a9337
0rigin_Robot <legolandisawesome@gmail.com>
2022-06-06 14:31:04 -0700

    Shooter commits

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import static frc.robot.Constants.*;
+import static frc.robot.ShooterTable.*;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
