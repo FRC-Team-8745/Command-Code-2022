08105f8de4b7262308adbb5cd4c878caf9d00597
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-06-06 14:31:48 -0700

    add linear interpolator

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,44 +6,53 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 // Intake CAN ID = 4
 public class Intake extends SubsystemBase {
-// Calling our variables
-  private final CANSparkMax m_intake = new CANSparkMax(4, MotorType.kBrushless);
-  //private final RelativeEncoder m_intakeEncoder = m_intake.getEncoder();
-  private State state = State.OFF;
-    public enum State {
-        IN(0.5),
-        EX(-1),
-        OFF(0);
-        public double speed;
-        private State(double speed){
-            this.speed = speed;
-        }
-    }
-  public Intake() {
-    m_intake.setOpenLoopRampRate(0.5);
-    m_intake.setIdleMode(IdleMode.kCoast);
-    m_intake.restoreFactoryDefaults();
-  }
+	// Calling our variables
+	private final CANSparkMax m_intake = new CANSparkMax(4, MotorType.kBrushless);
+	// private final RelativeEncoder m_intakeEncoder = m_intake.getEncoder();
+	private State state = State.OFF;
 
-  @Override
-  public void periodic() {
-    m_intake.set(state.speed);
+	public enum State {
+		IN(0.5),
+		EX(-1),
+		OFF(0);
 
-    // This method will be called once per scheduler run
+		public double speed;
 
-  }
-  public void setState(State state){
-      this.state = state;
-  }
-  public void setSpeed(double speed){
-      m_intake.set(speed);
-  }
-  public void setVoltage(double volts){
-      m_intake.setVoltage(volts);
-  }
-  public void stop(){
-      m_intake.setVoltage(0);
-  }
+		private State(double speed) {
+			this.speed = speed;
+		}
+	}
+
+	public Intake() {
+		m_intake.setOpenLoopRampRate(0.5);
+		m_intake.setIdleMode(IdleMode.kCoast);
+		m_intake.restoreFactoryDefaults();
+	}
+
+	@Override
+	public void periodic() {
+		m_intake.set(state.speed);
+
+		// This method will be called once per scheduler run
+
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public void setSpeed(double speed) {
+		m_intake.set(speed);
+	}
+
+	public void setVoltage(double volts) {
+		m_intake.setVoltage(volts);
+	}
+
+	public void stop() {
+		m_intake.setVoltage(0);
+	}
 }
