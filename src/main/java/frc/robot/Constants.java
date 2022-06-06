13f795414c10b9251ae5009b9bbce5ea1b9c087e
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2022-06-06 14:24:56 -0700

    has errors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,15 +1,80 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {}
+public class Constants {
+	/* Motor limits */
+
+	// Linear actuator
+	public static final double kLinearActuatorMin = 0.125;
+	public static final double kLinearActuatorMax = 1;
+
+	/* Gear Ratios */
+
+	// Drive gearbox ratio
+	public static final double kDriveGearbox = 10.71;
+
+	// Turret gear ratio
+	public static final int kTurretRatio = 120;
+
+	/* Shooter RPM calculation constants */
+
+	// The shooter speed while shooting from the fender
+	public static final double kFenderSpeed = 3000;
+	// Max battery voltage
+	public static final double kbatteryMax = 13;
+	// The max RPM of the shooter motor
+	public static final double kmaxRPM = 5500;
+
+	/* Auto Constants */
+
+	// The default auto program to run
+	public static final int kDefaultAuto = 0;
+
+	// The distances to drive in the two autos that collect cargo
+	public static final double kNormalAutoDriveDistance = 7.583;
+
+	/* AutoCommands PID */
+	public static final double kP = 0.2;
+	public static final double kI = 0.0;
+	public static final double kD = 0.0;
+
+	/* Drive Wheel Diameter */
+	public static final double kDiameter = 6;
+
+	/* Limelight Constants */
+
+	// Limelight mounting angle from perfectly vertical
+	public static final double kLimelightMountAngleDegrees = 34.45;
+
+	// Distance from the center of the Limelight lens to the floor
+	public static final double kLimelightLensHeight = 45;
+
+	/* Field Constants */
+
+	// Vision tape height
+	public static final double kVisionTapeHeight = 101.625;
+
+	/* Odometry Constants */
+
+	public static final double kHubCenterX = 8;
+	public static final double kHubCenterY = 4;
+	
+	// Default starting position
+	
+	
+	public static final double kRobotStartPosX = 6.951;
+	public static final double kRobotStartPosY = 2.947;
+	public static final double kRobotStartRotY = -155;
+	
+	
+	public static final double shooterSpeedOffset = 0;
+	
+	// Start position
+	/*
+	public static final double kRobotStartPosX = 6.5;
+	public static final double kRobotStartPosY = 2.8;
+	public static final double kRobotStartRotY = -154;
+	*/
+
+	// Auto code to run
+	public static final int autoToRun = 0;
+}
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,44 +6,58 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 // Intake CAN ID = 4
 public class Shooter extends SubsystemBase {
-// Calling our variables
-  private final CANSparkMax m_shooter = new CANSparkMax(4, MotorType.kBrushless);
-  //private final RelativeEncoder m_shooterEncoder = m_shooter.getEncoder();
-  private State state = State.OFF;
-    public enum State {
-        SHOOT(0.5),
-        UNLOAD(-0.25),
-        OFF(0);
-        public double speed;
-        private State(double speed){
-            this.speed = speed;
-        }
-    }
-  public Shooter() {
-    m_shooter.setOpenLoopRampRate(0.5);
-    m_shooter.setIdleMode(IdleMode.kCoast);
-    m_shooter.restoreFactoryDefaults();
-  }
+	// Calling our variables
+	private final CANSparkMax m_shooter = new CANSparkMax(4, MotorType.kBrushless);
+	// private final RelativeEncoder m_shooterEncoder = m_shooter.getEncoder();
+	private State state = State.OFF;
 
-  @Override
-  public void periodic() {
-    m_shooter.set(state.speed);
+	public enum State {
+		SHOOT(0.5),
+		UNLOAD(-0.25),
+		OFF(0);
 
-    // This method will be called once per scheduler run
+		public double speed;
 
-  }
-  public void setState(State state){
-      this.state = state;
-  }
-  public void setSpeed(double speed){
-      m_shooter.set(speed);
-  }
-  public void setVoltage(double volts){
-      m_shooter.setVoltage(volts);
-  }
-  public void stop(){
-      m_shooter.setVoltage(0);
-  }
+		private State(double speed) {
+			this.speed = speed;
+		}
+	}
+
+	public Shooter() {
+		m_shooter.setOpenLoopRampRate(0.5);
+		m_shooter.setIdleMode(IdleMode.kCoast);
+		m_shooter.restoreFactoryDefaults();
+	}
+
+	@Override
+	public void periodic() {
+		m_shooter.set(state.speed);
+
+		// This method will be called once per scheduler run
+
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	// Sets the RPM of the shooter motor
+	public void setRPM(double rpm) {
+		shooter.setVoltage((rpm / kmaxRPM) * kbatteryMax);
+	}
+
+	public void setSpeed(double speed) {
+		m_shooter.set(speed);
+	}
+
+	public void setVoltage(double volts) {
+		m_shooter.setVoltage(volts);
+	}
+
+	public void stop() {
+		m_shooter.setVoltage(0);
+	}
 }
